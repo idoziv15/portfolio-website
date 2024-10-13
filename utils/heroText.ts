e8c4320653c26e0bc5bf6c40ac1521ef2eeb447d
idoziv <idoziv56@gmail.com>
2024-10-13 14:51:10 +0300

    changed hetroText

--- utils/heroText.ts
+++ utils/heroText.ts
@@ -1,6 +1,6 @@
 export const heroText = {
   title: "Ido Ziv ;",
-  subtitle: "Junior Software Developer",
-  description: "Junior Developer creating innovative software by day and immersing in the world of tech and AI by night.",
+  subtitle: "Software Developer",
+  description: "Developer creating innovative software by day and immersing in the world of tech and AI by night.",
 };
 
